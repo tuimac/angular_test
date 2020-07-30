1d5f59b0aec9b294e82e198a3e85c1e143ff7603
tuimac <tuimac.devadm01@gmail.com>
2020-07-31 00:04:02 +0900

    delete api key

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ess=wg16h$0%wae-3%6+nvgt--wqhk*f+!tb7-a(^ud$3s98v2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
