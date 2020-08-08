baeb5a1b4330b4907112740be7e437ab22c4180c
tuimac <tuimac.devadm01@gmail.com>
2020-08-08 16:15:39 +0900

    fixed

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,26 @@
-var http = require('http'), require('url');
+var http = require('http');
 
-function main(){
-  let port = 8000
+var port = 8000
 
-  http.createServer(function(request, response){
-    const query = url.parse(request.url)
-    console.log(request);
-  }).listen(port);
-}
-
-main()
+http.createServer(function (req, res) {
+  if (req.method == 'POST') {
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk.toString();
+    });
+    req.on('end', () => {
+      res.end('ok');
+    });
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('Your input is ' + body + '.');
+    res.end();
+  } else if (req.method == 'GET') {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('Success to send GET!!');
+    res.end();
+  } else {
+    res.writeHead(405, {'Content-Type': 'text/plain'});
+    res.write('Method(' + req.method + ') is not allowed!!!');
+    res.end();
+  }
+}).listen(port);
